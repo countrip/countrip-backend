71459264726f16f68e18f531fd41a405bde27abf
hyeinisfree <kimhyein7110@gmail.com>
2023-07-04 19:37:11 +0900

    init

---
+++ src/main/java/com/countrip/countrip/CountripApplication.java
@@ -0,0 +1,13 @@
+package com.countrip.countrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/countrip/countrip/CountripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.countrip.countrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
